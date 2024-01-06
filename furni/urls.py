8728ac003aae772ee2421598224184e8e40b9c96
Misha <bdemlolp234@gmail.com>
2024-01-06 19:42:33 +0200

    latest update

---
+++ furni/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from furni.views import index, about, shop, thanks,  services, cart, blog, IndexView, ReserveView
+
+
+app_name = 'main'
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('about/', about, name='about'),
+    path('shop/', shop, name='shop'),
+    path('services/', services, name='services'),
+    path('contact/', IndexView.as_view(), name='contact'),
+    path('cart/', cart, name='cart'),
+    path('blog/', blog, name='blog'),
+    path('checkout/', ReserveView.as_view(), name='checkout'),
+    path('successful_order/', thanks, name='successful_order'),
+
+]
